8633b42b1de1bbfef3b41eba8ca8279c911c4278
DavidSaniez <david.saniez.pro@gmail.com>
2020-12-18 16:38:50 +0100

    unsubscribe fixed

--- src/app/core/services/itune-service.ts
+++ src/app/core/services/itune-service.ts
@@ -3,20 +3,28 @@ import { Inject, Injectable } from "@angular/core";
 import { Music } from "../models/music";
 import { map } from 'rxjs/operators';
 import { AppConfig, APP_CONFIG } from "src/app/app-config/app-config.module";
+import { Subscription } from "rxjs";
 
 @Injectable()
 export class ItuneService {
 
     public music: Music[];
     public query: string;
+    musicsSubscription: Subscription;
 
     constructor(private http: HttpClient, @Inject(APP_CONFIG) private config: AppConfig) {
 
     }
 
+    ngOnDestroy() {
+        if (this.musicsSubscription) {
+            this.musicsSubscription.unsubscribe();
+          }
+    }
+
     public searchMusic (queryTitle: string) {
         this.query = queryTitle;
-        this.http.get(`${this.config.apiEndpoint}search?term=${this.query}`).pipe(
+        this.musicsSubscription = this.http.get(`${this.config.apiEndpoint}search?term=${this.query}`).pipe(
             map(data => {
                 const res: any = data;
                 return res.results ? res.results : [];
